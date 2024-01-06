3370558778bb83f2337f4255c7cca05676cf512a
Bostan Ion <66279706+IBostanV@users.noreply.github.com>
2024-01-07 01:04:00 +0200

    PQ-79 Add image for the category entity (#23)
    Co-authored-by: Ion Bostan <i.bostan@intetics.com>

--- api/category/save.ts
+++ api/category/save.ts
@@ -1,10 +1,23 @@
-import request, { POST } from '../../utils/request';
-import { CATEGORY_PATH } from '../constant';
+import request, {POST} from '../../utils/request';
+import {CATEGORY_PATH} from '../constant';
 
-const saveCategory = (body) => request(CATEGORY_PATH, {
-  body,
-  method: POST,
-  withHeaders: true,
-});
+const saveCategory = (body, attachment) => {
+  const formData = new FormData();
+
+  formData.append('request', new Blob([JSON.stringify(body)], { type: 'application/json' }));
+
+  if (attachment) {
+    formData.append('attachment', attachment, attachment.name);
+  }
+
+  return request(CATEGORY_PATH, {
+    body: formData,
+    method: POST,
+    withHeaders: true,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    }
+  });
+}
 
 export default saveCategory;
--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  compiler: {
+    styledComponents: true,
+  }
 };
